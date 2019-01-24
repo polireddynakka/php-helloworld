c9739f9e62a637bb61b8fd6dee2eb1354a1972c0
polireddynakka <polireddynakka@gmail.com>
2019-01-24 13:08:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World this is pipeline job using jenkins1 the pipeline job was successfully implemented</p>'; ?> 
+ <?php echo '<p>Hello World this is pipeline job using jenkins1 the pipeline job was successfully implemented Thanks </p>'; ?> 
  </body>
 </html>
